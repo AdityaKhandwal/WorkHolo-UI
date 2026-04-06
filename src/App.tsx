eab67056a8f7c80bd137ffa96049c7327b8cca5c
AdityaKhandwal <adityakhandual5@gamil.com>
2026-04-06 16:10:14 +0530

    home page was not rendering, resolved the issues

--- src/App.tsx
+++ src/App.tsx
@@ -96,6 +96,7 @@ export default function App() {
       <Navbar />
       <Routes>
         {/* MAIN ROUTES */}
+        <Route path="/" element={<Home />} />
         <Route path="/Home" element={<Home />} />
         <Route path="/portfolio" element={<Portfolio />} />
         <Route path="/careers" element={<Page name="Careers" />} />
